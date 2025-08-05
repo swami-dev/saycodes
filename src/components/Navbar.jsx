44cdc9686b980a40d3dacab52ba68818595c8af0
swami-dev <blockswitching@gmail.com>
2025-08-05 19:08:32 +0530

    colour changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-warning fw-bold">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">SayCodes</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
